803217acf8e343568479d70236fe915ac3f30c51
jimrock2548 <jimrock2548@gmail.com>
2026-01-29 21:02:54 +0700

    feat : add Schema

--- controllers/dayoffController.js
+++ controllers/dayoffController.js
@@ -3,8 +3,8 @@ const getUserFromHeader = require("../utils/token");
 
 
 exports.requestDayOff = async (req,res) => {
-    //const { leaveType , reason , startDate , endDate , days } = req.body;
+    const { leaveType , reason , startDate , endDate , days } = req.body;
 
     const user = await getUserFromHeader(req);
-    console.log (user)
+    
 }
